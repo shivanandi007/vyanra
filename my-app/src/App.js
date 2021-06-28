2a07e9780d5a14084aade2301d67f6924344f932
shivanandi007 <shivanandi880@gmail.com>
2021-06-29 01:37:08 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './Capture2 (1).PNG';
+import './App.css';
+
+function App() {
+  return (
+    
+      <div>
+        <button class="btn success">BECOME A SELLER</button>
+<button class="btn info">LOG IN</button>
+
+       </div>
+    
+  );
+}
+
+export default App;
